9f4433920832486eb237a3154497b1946a083810
cgcreate <info@cgcreate.xyz>
2021-06-29 13:34:05 -0700

    Initial commit from gatsby: (https://github.com/cgcreatexyz/GATSBY-STARTER-HELLO-BJS-BASIC.git)

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SceneComponent from "../components/SceneComponent/SceneComponent";
+import {onSceneReady, onRender} from '../components/sampleScene/sampleScene';
+
+export default function Home() {
+  return (
+    <div className="main">
+      <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id='my-canvas' />
+    </div> 
+  )
+}
+
+
